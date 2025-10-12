6c530371f601c55e4d389ec713e107bf4e7eeeb7
BijoyDeep11 <bijoydeep007@gmail.com>
2025-10-12 09:50:44 +0530

    Add files via upload

---
+++ 01_basics.js
@@ -0,0 +1,13 @@
+const accountId = 1
+let accountEmail = "rajesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+//accountId = 2 // not allowed
+
+accountEmail = "ram@google.com"
+accountPassword = "19807"
+accountCity = "Bangalore"
+//console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
