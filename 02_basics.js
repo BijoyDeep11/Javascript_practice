fc063ed6a28d0b37d5a01d93897847d57be51f0c
Bijoy Deep Ghosh <bijoydeep007@gmail.com>
2025-10-13 19:59:02 +0530

    Add files via upload

---
+++ 02_basics.js
@@ -0,0 +1,25 @@
+"use strict";
+//alert(3+3) // we are using nodejs
+console.log(3
+    +
+    3)//code readability should be high
+
+console.log("Hitesh");
+
+let name = "Bijoy"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "hitesh");
+console.log(typeof "age");
